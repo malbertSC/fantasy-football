3a6cf7fa37f11061f6e178fc49d9af3bc90f5ef9
Matthew Albert <matthew.albert@gatherhere.com>
2018-12-11 22:06:37 -0500

    fix lerna, add xml-js

--- packages/devops/src/build-exposable-schema.ts
+++ packages/devops/src/build-exposable-schema.ts
@@ -13,6 +13,20 @@ function main() {
     console.log("api-schema.graphql built")
 }
 
+// jank
+// rfc: https://github.com/prisma/prisma/issues/2999
+function makeCollectionElementRequired(element: string) {
+    function addRequired(str, p1) {
+        const groups = str.split(p1);
+        return p1 + "!" + groups[1];
+    }
+    const regex = /: (\[\w*)\]!/g;
+    return element.replace(regex, addRequired);
+}
+
+// tslint:disable-next-line:max-line-length
+const test = makeCollectionElementRequired("leagues(where: LeagueWhereInput, orderBy: LeagueOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [League]!");
+console.log(test)
 function getAPISafeSchema(introspectionResult) {
     const apiTypes = introspectionResult.__schema.types
         .filter(type => !["Subscription", "PageInfo"].includes(type.name))
@@ -26,6 +40,32 @@ function getAPISafeSchema(introspectionResult) {
         );
     const queryType = apiTypes.find(type => type.name === "Query");
     queryType.fields = queryType.fields.filter(type => !type.name.includes("Connection"));
+    queryType.fields = queryType.fields.map(field => {
+        if (field.type.ofType && field.type.ofType.kind === "LIST") {
+            const insideType = {...field.type.ofType.ofType}
+            const newField = {
+                ...field,
+                ...{
+                    type: {
+                        ofType: {
+                            kind: "NON_NULL",
+                            name: null,
+                            ofType: insideType
+                        }
+                    }
+                }
+            }
+            // const newField = {...field};
+            // newField.type.ofType.ofType.ofType = {
+            //     kind: "NON_NULL",
+            //     name: null,
+            //     ofType: null
+            // };
+            console.log(newField.type.ofType);
+            return newField
+        }
+        return field;
+    });
     const mutationType = apiTypes.find(type => type.name === "Mutation");
     mutationType.fields = mutationType.fields.filter(type => !(
         type.name.includes("Nfl") || type.name === "executeRaw"
